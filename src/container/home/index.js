aeddea337e2ffb100aee60f3bbf895adaea9712b
akinolu52 <akinolu52@gmail.com>
2018-05-19 09:41:32 -0700

    front-end

--- src/container/home/index.js
+++ src/container/home/index.js
@@ -2,8 +2,36 @@ import React from 'react';
 
 const Home = () => (
   <div style={{ marginTop: 50 }}>
-    <h1>Home Page</h1>
-    <p>Did you get here via Redux?</p>
+    <div className="features">
+            <h1>
+                this is our tagline
+            </h1>
+            <div className="feature">
+                <i className="fa fa-person fa-4x"></i>
+                <h3>Logistics</h3>
+                <p>We wash your cloth</p>
+            </div>
+            <div className="feature">
+                <i className="fa fa-person fa-4x"></i>
+                <h3>Washing</h3>
+                <p>We wash your cloth</p>
+            </div>
+            <div className="feature">
+                <i className="fa fa-person fa-4x"></i>
+                <h3>delivery</h3>
+                <p>We wash your cloth</p>
+            </div>
+            <div className="feature">
+                <i className="fa fa-person fa-4x"></i>
+                <h3>Ironing</h3>
+                <p>We wash your cloth</p>
+            </div>
+            <div className="feature">
+                <i className="fa fa-person fa-4x"></i>
+                <h3>Drying</h3>
+                <p>We wash your cloth</p>
+            </div>
+        </div>
   </div>
 );
 
