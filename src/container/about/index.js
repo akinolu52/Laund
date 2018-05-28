da212431b241df387d74e33ee7c267797bdaa52d
akinolu52 <akinolu52@gmail.com>
2018-05-28 03:14:37 -0700

    sme

--- src/container/about/index.js
+++ src/container/about/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const About = () => (
-  <div style={{ marginTop: 50 }}>
+  <div style={{ marginTop: 50, textAlign: 'center' }}>
     <h1>About Page</h1>
     <p>Did you get here via Redux?</p>
   </div>
--- src/partials/header.js
+++ src/partials/header.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 
-const Header = () => (
-    <header>
-        <nav>
-            <Link to="/">Laund It</Link>
-            <Link to="/about-us">About</Link>
-            <Link to="/">Sign in | up</Link> 
-        </nav>
-        <h1>Laund-It: Wash it chaper and get it to you faster.</h1>
-        <Link to="/" className="btnOutline">Pricing</Link>
-    </header> 
-);
+// const check = withRouter(props => <Header {...props} />);
+// pathname
+// const Header = () => (
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // this.getPath();
+    }
+    render ()  {
+        return (
+            <header>
+                <nav>   X
+                    <Link to="/">Laund It</Link>
+                    <Link to="/about-us">About</Link>
+                    <Link to="/">Sign in | up</Link> 
+                </nav>
+                <div >
+                    <h1>{ this.props.location }Laund-It: Wash it chaper and get it to you faster.</h1>
+                    <Link to="/" className="btnOutline">Pricing</Link>
+                </div>
+                <div >
+                    {/* <form>
+                        <input type="text" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <input type="submit" value="Sign In" />
+                    </form> */}
+                    {/* <form>
+                        <input type="text" placeholder="Full Name" />
+                        <input type="text" placeholder="Email" />
+                        <input type="tel" placeholder="Phone Number" />
+                        <input type="password" placeholder="Password" />
+                        <input type="submit" value="Sign Up" />
+                    </form> */}
+                </div>
+            </header> 
+        )
+    }
+};
 
 export default Header;
